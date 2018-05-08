35d77e935ab523f2866d0ff9e6e48372f0d7b219
Maksym Akulov <maksakulov@hotmail.com>
2018-05-08 16:01:42 +0300

    Add class Transaction(fixes #4)

---
+++ Parking/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking
+{
+    public class Transaction
+    {
+        public DateTime TransactionDataTime { get; set; }
+        public int CarId { get; set; }
+        public int MoneyPaid { get; set; }
+    }
+}
