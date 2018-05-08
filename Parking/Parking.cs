6f995f7bbe30e68e14841efb8dc4099a8737098b
Maksym Akulov <maksakulov@hotmail.com>
2018-05-08 17:30:39 +0300

    Add class Parking

---
+++ Parking/Parking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking
+{
+    public class Parking
+    {
+        public List<Car> CarList { get; set; }
+        public List<Transaction> TransactionList { get; set; }
+        public int Balance { get; set; }
+    }
+}
